f3929e998e440a061a41cdc77de646effb75463a
Khushal Prajapat <khushalverse@gmail.com>
2026-03-14 17:43:57 +0530

    fix model to 2.0-flash

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -173,7 +173,7 @@ export default function ChatPage() {
     const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
     const url =
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
     abortControllerRef.current = new AbortController();
 
